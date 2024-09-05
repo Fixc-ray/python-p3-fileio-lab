8f8b662f737c21b08cf9f35b35aece4a26018125
Justin <rayjustin481@gmail.com>
2024-09-05 08:59:21 +0300

    done

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,15 @@
 def write_file(file_name, file_content):
-    pass
+    with open(str(file_name) + "txt",mode = "w") as file:
+        file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    with open(str(file_name) + "txt", mode="a") as file:
+        file.write(append_content)
 
 def read_file(file_name):
-    pass
+    with open(str(file_name) + "txt", mode="r") as file:
+        print(file.read())
+        
+write_file(file-name, file_content)
+append_file(file-name, file-content)
+read_file(file_name)
